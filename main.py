116591b341fc314cf57dd820d925e74e7e2fb57d
Elliott Escalante <48373238+Satchel05@users.noreply.github.com>
2023-12-21 21:19:32 -0800

    refactor for status codes

--- main.py
+++ main.py
@@ -2,19 +2,14 @@ import requests
 
 word = input()
 
-try:
-    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
-    response = requests.get(url)
+url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
+response = requests.get(url)
+
+if response.status_code != 200:
+    print("Not a word")
+else:
     response = response.json()
-
-    if response:
-        w = response[0]['word']
-        definition = response[0]['meanings'][0]["definitions"][0]["definition"]
-        print(f"Definition of {w} is...\n")
-        print(definition)
-
-    else:
-        print("Something is wrong with the API :(")
-
-except Exception as excpt:
-    print(f"Something is wrong, but not with the API: {excpt}")
+    w = response[0]['word']
+    definition = response[0]['meanings'][0]["definitions"][0]["definition"]
+    print(f"Definition of {w} is...\n")
+    print(definition)
