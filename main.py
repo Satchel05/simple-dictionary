931108166a40c44b804ad631e8757171f1108bc4
Elliott Escalante <48373238+Satchel05@users.noreply.github.com>
2023-12-21 18:29:11 -0800

    print a word and its definition

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import requests
 
+word = input()
+
 try:
-    url = "https://api.dictionaryapi.dev/api/v2/entries/en/python"
+    url = f"https://api.dictionaryapi.dev/api/v2/entries/en/{word}"
     response = requests.get(url)
+    response = response.json()
 
     if response:
-        print("Success!")
+        w = response[0]['word']
+        definition = response[0]['meanings'][0]["definitions"][0]["definition"]
+        print(f"Definition of {w} is...\n")
+        print(definition)
+
     else:
         print("Something is wrong with the API :(")
 
-except:
-    print(f"Something is wrong, but not with the API")
-
+except Exception as excpt:
+    print(f"Something is wrong, but not with the API: {excpt}")
